a9ed64ec8551fb184b3d0820a0abb26e2c0109a3
Deniss <cvetkovsdeniss4@gmail.com>
2025-09-24 11:47:31 +0300

    Added book review addition to db

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 
 <?php
 
-echo "Dati veiksmīgi nosutīti!";
+echo "<br>Dati veiksmīgi nosutīti!<br>";
 
 $servername = "localhost";
 $username = "book_review_user_24092025";
@@ -16,4 +16,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully<br>";
+
+$fullName = $_POST["name"] . " " . $_POST["surname"];
+$bookTitle = $_POST["title"];
+$reviewText = $_POST["review"];
+$rating = $_POST["rating"];
+$query = "INSERT INTO book_review (full_name, book_title, review_text, rating) VALUES ('$fullName','$bookTitle','$reviewText','$rating')";
