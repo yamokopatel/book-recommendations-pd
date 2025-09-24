451aae3b39d698e8b33a59fa40736e926906b9e6
Deniss <cvetkovsdeniss4@gmail.com>
2025-09-24 11:33:40 +0300

    Added PHP to SQL connection

--- submit.php
+++ submit.php
@@ -4,3 +4,16 @@
 
 echo "Dati veiksmīgi nosutīti!";
 
+$servername = "localhost";
+$username = "book_review_user_24092025";
+$password = "password";
+$dbname = "book_review_24092025";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
